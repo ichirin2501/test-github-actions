bdf91dc603a01554abd826d766de3219f61a6ca4
ichirin2501 <ichirin.2501@gmail.com>
2020-10-18 02:13:47 +0900

    edit-mode

--- sql/user.sql
+++ sql/user.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS users (
   id INT UNSIGNED NOT NULL,
   name VARCHAR(255) NOT NULL,
   created  DATETIME NOT NULL,
+  updated  DATETIME NOT NULL,
   PRIMARY KEY (id),
   KEY idx_created (created),
   KEY idx_name (name)
