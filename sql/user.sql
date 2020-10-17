d2c7591317fd562b99a8f89f42535ef1a7df6773
ichirin2501 <ichirin.2501@gmail.com>
2020-10-17 19:23:36 +0900

    add index

--- sql/user.sql
+++ sql/user.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS users (
   name VARCHAR(255) NOT NULL,
   created  DATETIME NOT NULL,
   PRIMARY KEY (id),
+  KEY idx_name (name),
   KEY idx_created (created)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
