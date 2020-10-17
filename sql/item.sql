a9eb54056cc869dbfe1eff7be7b6accec07f57f0
ichirin2501 <ichirin.2501@gmail.com>
2020-10-18 02:05:55 +0900

    add idx

--- sql/item.sql
+++ sql/item.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS items (
   id INT UNSIGNED NOT NULL,
   name VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  KEY idx_name (name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
