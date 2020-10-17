c445db8529ecc40c72a69379ec8bc7bf46fa77b6
ichirin2501 <ichirin.2501@gmail.com>
2020-10-17 19:38:52 +0900

    rm index

--- sql/user.sql
+++ sql/user.sql
@@ -2,7 +2,5 @@ CREATE TABLE IF NOT EXISTS users (
   id INT UNSIGNED NOT NULL,
   name VARCHAR(255) NOT NULL,
   created  DATETIME NOT NULL,
-  PRIMARY KEY (id),
-  KEY idx_name (name),
-  KEY idx_created (created)
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
