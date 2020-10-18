07b2bfa5b98b97bb99dc8f23da567c8aba697484
ichirin2501 <ichirin.2501@gmail.com>
2020-10-18 22:23:57 +0900

    add table

--- sql/user.sql
+++ sql/user.sql
@@ -6,7 +6,12 @@ CREATE TABLE IF NOT EXISTS users (
   KEY idx_created (created)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS special_users (
-  id INT UNSIGNED NOT NULL,
-  name VARCHAR(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS user_nanika (
+  id int(11) unsigned NOT NULL AUTO_INCREMENT,
+  start_at timestamp NOT NULL,
+  end_at timestamp NOT NULL,
+  description varchar(256) DEFAULT NULL,
+  created timestamp NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
